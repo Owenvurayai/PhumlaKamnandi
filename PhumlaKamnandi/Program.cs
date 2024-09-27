d93bce49cfa54647ed0dbf3825aab0450761816c
Kholisile Ndzube <ndzubekholisile@gmail.com>
2024-09-27 03:00:15 +0200

    Please gents fix the errors in the Business Logic, All the files that were there are moved to Business_No_Uploading_Not_Working_Code Folder

--- PhumlaKamnandi/Program.cs
+++ PhumlaKamnandi/Program.cs
@@ -1,3 +1,4 @@
+using PhumlaKamnandi.Presentation;
 namespace PhumlaKamnandi
 {
     internal static class Program
@@ -11,7 +12,7 @@ namespace PhumlaKamnandi
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Home());
         }
     }
 }
