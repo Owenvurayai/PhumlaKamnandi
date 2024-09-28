b70261e00652f1b0a2ed39749351e1125d1a52c1
ndzubekholisile <68696727+ndzubekholisile@users.noreply.github.com>
2024-09-28 16:47:18 +0200

    Renamed all forms

---
+++ PhumlaKamnandi/Presentation/LoginForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhumlaKamnandi.Presentation
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            HomeForm h = new HomeForm();
+            h.Show();
+            this.Visible = false;
+        }
+    }
+}
